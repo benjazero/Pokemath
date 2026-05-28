97e6bc61e7b59a84d5e61e0e058292a02040625e
amambai <benjaminesquivelsilva232@gmail.com>
2026-05-28 19:28:43 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 #include <ctime>
 #include "Menu.h"
 
+// Archivo principal del programa.
+// Inicializa valores aleatorios y ejecuta
+// el menú principal del juego.
+
 int main() {
     srand(static_cast<unsigned int>(time(nullptr)));
     Menu menu;
